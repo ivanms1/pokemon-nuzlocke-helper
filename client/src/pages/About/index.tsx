88e274dcfe1bc3dc3286e5cdbc4f22d58faa3e1b
Ivan <ivanms1@gmail.com>
2020-01-22 23:49:16 -0300

    add link to website

--- client/src/pages/About/index.tsx
+++ client/src/pages/About/index.tsx
@@ -6,7 +6,16 @@ const About = () => {
   return (
     <div className={styles.About}>
       <h1>About</h1>
-      <h3>Created by Ivan</h3>
+      <h3>
+        Created by{' '}
+        <a
+          href='https://www.ivansaldano.com/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Ivan
+        </a>
+      </h3>
       <p>
         This is a gift to the pokemon community, free to use. Gotta catch'em
         all!
